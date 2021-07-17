0721a89b81dd1ed78b29368bf8f6dbce3ccd4dc6
Tauane Costa <tauanedasilvacosta@gmail.com>
2021-07-17 18:17:21 -0300

    First Commit

---
+++ src/main/java/com/xml/exercise/xmlexercise/XmlexerciseRestApplication.java
@@ -0,0 +1,11 @@
+package com.xml.exercise.xmlexercise;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class XmlexerciseRestApplication extends Application {
+}
